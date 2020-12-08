dd8613334e1f85b682379e780e3f2cd6e566399c
Saija Latvakangas <saijalatvakangas@gmail.com>
2020-12-08 13:36:47 +0200

    add data for to do list

---
+++ src/components/todoData.js
@@ -0,0 +1,28 @@
+const todoData = [
+  {
+    id: 1,
+    text: "Suorita JavaScript-kurssi",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "Suorita Java-kurssi",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Suorita React-kurssi",
+    completed: true
+  },
+  {
+    id: 4,
+    text: "Pese koirat",
+    completed: false
+  },
+  {
+    id: 5,
+    text: "Käännä unirytmi",
+    completed: false
+  },
+]
+export default todoData;
